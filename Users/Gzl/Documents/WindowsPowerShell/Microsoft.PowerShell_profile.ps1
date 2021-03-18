ce3df19a63ff387c654551915cac986453941966
flyxyz123 <gky44px1999@gmail.com>
2021-03-17 19:54:17 -0700

    Revert "change dir"
    This reverts commit 5664f460b57e1d94ee247b10ea0cd6a35f11d14b.

---
+++ Users/Gzl/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+cls
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Function config 
+{
+  git --git-dir=G:\ComputerStudy\configs\.myconfigs\ --work-tree=C:\ $args
+}
