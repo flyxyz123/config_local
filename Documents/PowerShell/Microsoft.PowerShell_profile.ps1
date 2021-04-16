946b97c55facbdd7530ea50efdaa1c64f0bd6c6d
flyxyz123 <gky44px1999@gmail.com>
2021-04-16 00:35:47 -0700

    + cd to .lnk @ profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,27 @@ Function cfgl {
 Function cfgc {
     git --git-dir=G:\ComputerStudy\configs\.mycfg_cross_platform\ --work-tree=$HOME\ $args
 }
+
+# https://github.com/ajeetdsouza/zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
+
+# https://superuser.com/a/810991
+# can't use wildcard and autocomplete for .lnk
+remove-item alias:cd -force
+function cd($target)
+{
+    if($target.EndsWith(".lnk"))
+    {
+        $sh = new-object -com wscript.shell
+		$fullpath = (resolve-path $target).Path
+		#$fullpath = resolve-path $target
+        $targetpath = $sh.CreateShortcut($fullpath).TargetPath
+        set-location $targetpath
+    }
+    else {
+        set-location $target
+    }
+}
